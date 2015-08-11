70257b7e602bcb12fed81f438bb2a15606dc0669
Raymond Benc <r@phpfox.com>
2015-08-11 14:23:49 +0200

    Automated Dist: 1439295824

--- start.php
+++ start.php
@@ -12,8 +12,53 @@ new Core\Route('/admincp/build-stock-users', function(Core\Controller $controlle
 			$val['total'] = 1000;
 		}
 
-		// $response = file_get_contents('http://www.pexels.com/search/summer.js?page=1');
-		// preg_match_all('/https:\/\/static\.pexels\.com\/photos\/([0-9]+)\/([a-zA-Z0-9-]+).jpg/i', $response, $matches);
+		$createContent = (!empty($val['admin_id']) ? true : false);
+
+		if ($createContent) {
+			$adminId = (int) $val['admin_id'];
+			$tmp = PHPFOX_DIR_FILE . 'static/';
+			$photos = [];
+			$response = file_get_contents('http://www.pexels.com/search/summer.js?page=1');
+			preg_match_all('/https:\/\/static\.pexels\.com\/photos\/([0-9]+)\/([a-zA-Z0-9-]+).jpg/i', $response, $matches);
+			define('PHPFOX_HTML5_PHOTO_UPLOAD', true);
+			foreach ($matches[0] as $url) {
+				$parts = explode('/', $url);
+				$name = ucwords(str_replace('-', ' ', explode('.', $parts[count($parts) - 1])[0]));
+				$path = $tmp . md5($url) . '.jpg';
+				file_put_contents($path, file_get_contents($url));
+				register_shutdown_function(function () use ($path) {
+					if (file_exists($path)) {
+						unlink($path);
+					}
+				});
+				$photos[] = [
+					'name' => md5($url) . '.jpg',
+					'title' => $name,
+					'path' => $path
+				];
+			}
+
+			/**
+			 * @copyright http://commments.com/
+			 */
+			$comments = [
+				'This shot blew my mind.',
+				'It\'s fab not just fabulous!',
+				"This is delightful work m8",
+				"I think I'm crying. It's that amazing.",
+				"Incredibly thought out! I'm in!",
+				"Let me take a nap... great shot, anyway.",
+				"You just won the internet!",
+				"Very bold style mate",
+				"Mission accomplished. It's sleek :)",
+				"Contrast.",
+				"This is strong and excellent m8",
+				"Nice use of light in this shot!!",
+				"I want to learn this kind of shape! Teach me.",
+				"YEW!",
+				"Engaging work you have here."
+			];
+		}
 
 		$data = json_decode(file_get_contents('https://randomuser.me/api/?results=' . $val['total']));
 		try {
@@ -29,7 +74,7 @@ new Core\Route('/admincp/build-stock-users', function(Core\Controller $controlle
 
 				$ApiUser->assign([
 					'name' => ucwords($me->name->first) . ' ' . ucwords($me->name->last),
-					'email' => $me->email,
+					'email' => uniqid() . '.' . $me->email,
 					'password' => (empty($val['password']) ? uniqid() : $val['password'])
 				]);
 				$u = $ApiUser->post();
@@ -47,6 +92,105 @@ new Core\Route('/admincp/build-stock-users', function(Core\Controller $controlle
 				], ['user_id' => $u->id]);
 
 				\User_Service_Process::instance()->uploadImage($u->id, true, $me->picture->large, true);
+
+				if ($createContent) {
+					// Friend request to the admin
+					\Friend_Service_Request_Process::instance()->add($u->id, $adminId);
+
+					/*
+					\User_Service_Auth::instance()->setUserId($u->id);
+					$success = \Mail_Service_Process::instance()->add([
+						'to' => $adminId,
+						'message' => 'lorem ipsum...'
+					]);
+
+					if (!$success) {
+						throw new \Exception(implode(' ', \Phpfox_Error::get()));
+					}
+					*/
+
+					$photo = $photos[rand(0, (count($photos) - 1))];
+					$sHTML5TempFile = $photo['path'];
+					$fn = $photo['name'];
+					$_FILES['image'] = array(
+						'name' => array($fn),
+						'type' => array('image/jpeg'),
+						'tmp_name' => array($sHTML5TempFile),
+						'error' => array(0),
+						'size' => array(filesize($sHTML5TempFile))
+					);
+
+					$oFile = \Phpfox_File::instance();
+					if ($aImage = $oFile->load('image[0]', array(
+							'jpg',
+							'gif',
+							'png'
+						)
+					)
+					) {
+						$iId = \Photo_Service_Process::instance()->add($u->id, array_merge([
+							'title' => $photo['title']
+						], $aImage));
+
+						if ($sFileName = $oFile->upload('image[0]',
+							Phpfox::getParam('photo.dir_photo'),
+							$iId,
+							true
+						)
+						) {
+
+						} else {
+							throw new \Exception(implode('', \Phpfox_Error::get()));
+						}
+
+						// Get the current image width/height
+						$aSize = getimagesize(Phpfox::getParam('photo.dir_photo') . sprintf($sFileName, ''));
+
+						// Update the image with the full path to where it is located.
+						$aUpdate = array(
+							'destination' => $sFileName,
+							'width' => $aSize[0],
+							'height' => $aSize[1],
+							'server_id' => Phpfox_Request::instance()->getServer('PHPFOX_SERVER_ID'),
+							'allow_rate' => (empty($aVals['album_id']) ? '1' : '0'),
+							'description' => (empty($aVals['description']) ? null : $aVals['description'])
+						);
+
+						\Photo_Service_Process::instance()->update($u->id, $iId, $aUpdate);
+						$oImage = \Phpfox_Image::instance();
+						foreach (Phpfox::getParam('photo.photo_pic_sizes') as $iSize) {
+							// Create the thumbnail
+							if ($oImage->createThumbnail(Phpfox::getParam('photo.dir_photo') . sprintf($sFileName, ''), Phpfox::getParam('photo.dir_photo') . sprintf($sFileName, '_' . $iSize), $iSize, $iSize, true, ((Phpfox::getParam('photo.enabled_watermark_on_photos') && Phpfox::getParam('core.watermark_option') != 'none') ? (Phpfox::getParam('core.watermark_option') == 'image' ? 'force_skip' : true) : false)) === false) {
+								throw new \Exception(implode('', \Phpfox_Error::get()));
+							}
+						}
+
+						// Add a feed
+						$iFeedId = \Feed_Service_Process::instance()->add('photo', $iId, 0, 0, 0, $u->id);
+
+						// Comment and like the photo
+						$users = new \Api\User();
+						foreach ($users->get() as $user) {
+							\Like_Service_Process::instance()->add('photo', $iId, $user->id);
+
+							$comment = $comments[rand(0, (count($comments) - 1))];
+							\Comment_Service_Process::instance()->add([
+								'parent_id' => 0,
+								'type' => 'photo',
+								'item_id' => $iId,
+								'text' => $comment
+							], $user->id);
+						}
+
+
+						// Poke the admin
+						\User_Service_Auth::instance()->setUserId($u->id);
+						\Poke_Service_Process::instance()->sendPoke($adminId);
+
+					} else {
+						throw new \Exception(implode('', \Phpfox_Error::get()));
+					}
+				}
 			}
 		} catch (\Exception $e) {
 			return [
@@ -54,10 +198,22 @@ new Core\Route('/admincp/build-stock-users', function(Core\Controller $controlle
 			];
 		}
 
+		\Phpfox_Cache::instance()->remove();
+
 		return [
 			'success' => true
 		];
 	}
 
-	return $controller->render('build.html');
+	$db = new \Core\Db();
+	$options = [];
+	$admins = $db->select('*')->from(':user')->where(['user_group_id' => ADMIN_USER_ID])->all();
+	$options[0] = 'Select:';
+	foreach ($admins as $admin) {
+		$options[$admin['user_id']] = $admin['full_name'];
+	}
+
+	return $controller->render('build.html', [
+		'admins' => $options
+	]);
 });
